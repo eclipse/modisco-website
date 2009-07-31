feabe0a95ec4f50c069cfd7ee3b4a73143492b8f
gbarbier <gbarbier>
2009-07-31 13:09:27 +0000

    Factorisation of right column content.

--- index.php
+++ index.php
@@ -30,8 +30,8 @@
 	include('../news/scripts/news.php');
 	$modisconews = get_modisconews(7);
 		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+ob_start();
+?>
 
 	<!-- Middle part -->
 	<div id="midcolumn">
@@ -101,57 +101,18 @@ MoDisco aims at supporting these three phases by providing :
 	<hr/>
 		
 		<div class="homeitem"> <!-- homeitem3col -->
-			$modisconews
+<?= $modisconews?>			
 		</div>
 		<hr class="clearer" />
 	</div>
 
 <!-- Right Part -->
-<div id="rightcolumn">
-	<div class="sideitem">
-		<h6>Incubation</h6>
-   		<p>Some components are currently in their <a href="http://www.eclipse.org/projects/dev_process/validation-phase.php">Validation (Incubation) Phase</a>.</p> 
-	   	<div align="center">
-   			<a href="http://www.eclipse.org/projects/what-is-incubation.php">
-   				<img align="right" src="../resources/images/egg-incubation.png" border="0" alt="Incubation" />
-   			</a>
-   		</div>
-	</div>
-		
-		<div class="sideitem">
-			<h6>Getting Started</h6>
-			<ul>
-				<li><a href="doc/MoDisco_Flyer-Poster_1.0.pdf">Flyer-poster</a></li>
-				<li><a href="doc/MoDisco_Overview_1.0.pdf">Overview (slides)</a></li>
-				<li><a href="about.php">Description</a></li>
-				<li><a href="http://wiki.eclipse.org/index.php/MoDisco">Wiki</a></li>
-			</ul>
-		</div>
-		
-		<div class="sideitem">
-			<h6>Components</h6>
-			<ul>
-				<li><a href="useCases/">Use Cases</a></li>
-				<li><a href="technologies/">Technologies</a></li>
-				<li><a href="infrastructure/">Infrastructure</a></li>
-			</ul>
-		</div>
-		
-		<div class="sideitem">
-			<h6>Utilities</h6>
-			<ul>
-				<li><a href="http://www.eclipse.org/modeling/emf/">EMF (Model Handling)</a></li>
-				<li><a href="http://www.eclipse.org/m2m/atl/">ATL (Model Transformation)</a></li>
-				<li><a href="http://www.eclipse.org/gmt/amw/">AMW (Model Weaving)</a></li>
-				<li><a href="http://www.eclipse.org/gmt/am3/">AM3 (Megamodeling)</a></li>
-			</ul>
-		</div>
-		
-		<br>
-		
-	</div>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/gmt/modisco/right_column.php");
 
-EOHTML;
+
+$html = ob_get_contents();
+ob_end_clean();
 
 
 	# Generate the web page
---
+++ right_column.php
@@ -0,0 +1,34 @@
+<!-- factorized contents of right column for MoDisco -->
+<div id="rightcolumn">
+	<div class="sideitem">
+		<h6>Incubation</h6>
+   		<p>Some components are currently in their <a href="http://www.eclipse.org/projects/dev_process/validation-phase.php">Validation (Incubation) Phase</a>.</p> 
+	   	<div align="center">
+   			<a href="http://www.eclipse.org/projects/what-is-incubation.php">
+   				<img align="right" src="http://www.eclipse.org/gmt/resources/images/egg-incubation.png" border="0" alt="Incubation" />
+   			</a>
+   		</div>
+	</div>
+		
+		<div class="sideitem">
+			<h6>Getting Started</h6>
+			<ul>
+				<li><a href="http://www.eclipse.org/gmt/modisco/doc/MoDisco_Flyer-Poster_1.0.pdf">Flyer-poster</a></li>
+				<li><a href="http://www.eclipse.org/gmt/modisco/doc/MoDisco_Overview_1.0.pdf">Overview (slides)</a></li>
+				<li><a href="http://wiki.eclipse.org/index.php/MoDisco">Description</a></li>
+			</ul>
+		</div>
+		
+		<div class="sideitem">
+			<h6>Utilities</h6>
+			<ul>
+				<li><a href="http://www.eclipse.org/modeling/emf/">EMF (Model Handling)</a></li>
+				<li><a href="http://www.eclipse.org/m2m/atl/">ATL (Model Transformation)</a></li>
+				<li><a href="http://www.eclipse.org/gmt/amw/">AMW (Model Weaving)</a></li>
+				<li><a href="http://www.eclipse.org/gmt/am3/">AM3 (Megamodeling)</a></li>
+			</ul>
+		</div>
+		
+		<br>
+		
+</div>
