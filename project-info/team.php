dff08af50e7c94231eddd2b78598d111d4a6f9b2
gbarbier <gbarbier>
2009-11-03 17:00:16 +0000

    Try to fix team informations (bug in eclipse database or in modeling framework ?)

--- project-info/team.php
+++ project-info/team.php
@@ -17,6 +17,6 @@ ob_start();
 <li>Hugo Bruneliere</li>
 </ul>
 </div>
-<?php
-include_once $_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/team-common.php";
-?>
+<!--<?php-->
+<!--include_once $_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/team-common.php";-->
+<!--?>-->
