8a418f58d03fc85e0bcb12789174fb8f1a7817ee
gbarbier <gbarbier>
2009-11-03 16:58:51 +0000

    Try to fix team informations (bug in eclipse database or in modeling framework ?)

--- project-info/team.php
+++ project-info/team.php
@@ -1,4 +1,22 @@
 <?php
 $PR = "modeling/gmt/modisco";
+$PR = "modisco";
+ob_start();
+?>
+<div id="midcolumn">
+<img style="float:right" src="resources/logoMoDiscoSmall.jpg" alt="MoDisco Logo"/>
+<h1>Meet The MoDisco Team</h1>
+<h3>Contributors</h3>
+<ul>
+<li>Gabriel Barbier</li>
+<li>Nicolas Bros</li>
+<li>Hugo Bruneliere</li>
+</ul>
+<h3>Project leads</h3>
+<ul>
+<li>Hugo Bruneliere</li>
+</ul>
+</div>
+<?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/team-common.php";
 ?>
