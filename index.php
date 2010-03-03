df5c09a1f7c0e3ae99847145de699e11ce6c8886
hbrunelie <hbrunelie>
2010-03-03 17:18:15 +0000

    Minor editing on the home page

--- index.php
+++ index.php
@@ -96,14 +96,14 @@ MoDisco aims at supporting these three phases by providing :
     
     <table width="100%">
     	<tr>
-    		<td width="80%" align="center" valign="middle"> 
+    		<td width="90%" align="center" valign="middle"> 
     			The MoDisco project has been initiated and originally developed in the context of the <a href="http://www.modelplex.org">IST European MODELPLEX project</a> (MODELing solution for comPLEX software systems, FP6-IP 34081).
     		</td>
-    		<td> 
+    		<td width="10%"> 
     			<a href="http://www.modelplex.org"> <img align="right" src="./resources/modelplexLogo.JPG"/> </a>
     		</td>
     	</tr>
-    <table>
+    </table>
     
     <br /><br />
     
