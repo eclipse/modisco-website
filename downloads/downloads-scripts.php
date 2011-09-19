943464b315ce13fbb824dde091429f8077d5b011
nbros <nbros>
2011-09-19 13:14:59 +0000

    hide release until release day

--- downloads/downloads-scripts.php
+++ downloads/downloads-scripts.php
@@ -73,7 +73,7 @@ function getBuildsFromDirs() // massage the builds into more useful structures
 	{
 		foreach ($dirList as $dir)
 		{
-			//if($dir != 'R201106080421') { /* XXX hide release until release day */
+			if($dir != 'R201109150330') { /* XXX hide release until release day */
 				$ty = substr($dir, 0, 1); //first char
 
 				if ($sortBy != "date")
@@ -88,7 +88,7 @@ function getBuildsFromDirs() // massage the builds into more useful structures
 
 					$builds_temp[$a] = $b;
 				}
-			//}
+			}
 		}
 	}
 
