40ff6744b08289467dd6b6fdaf704cb9ff38950e
Hugo Bruneliere <hugo.bruneliere@inria.fr>
2014-06-24 14:07:03 +0200

    Update MoDisco homepage (temp)

--- index.php
+++ index.php
@@ -17,13 +17,6 @@
 	$App 	= new App();
 	$Nav	= new Nav();
 	$Menu 	= new Menu();
-	include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 
-	$App 	= new App();
-	$Nav	= new Nav();
-	$Menu 	= new Menu();	
 	include($App->getProjectCommon());
 
 	# Define these here, or in _projectCommon.php for site-wide values
