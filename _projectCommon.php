6a6f927f4591c24754712f8224b5bb2b8250c38e
Hugo Bruneliere <hugo.bruneliere@inria.fr>
2014-06-24 13:36:59 +0200

    Update MoDisco homepage

--- _projectCommon.php
+++ _projectCommon.php
@@ -11,30 +11,23 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->setLinkList(array());
-	$Nav->addCustomNav("About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=modeling.mdt.modisco", "", 1);
-	$Nav->addNavSeparator("MDT", "http://www.eclipse.org/mdt/");
-	$Nav->addCustomNav("Download", "http://www.eclipse.org/modeling/mdt/downloads/", "_self", 1);
-	$Nav->addCustomNav("Documentation", "http://www.eclipse.org/modeling/mdt/docs.php", "_self", 1);
-	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/MDT", "_blank", 1);
-	$Nav->addNavSeparator("MoDisco", "/MoDisco/");
-	$Nav->addCustomNav("Components", "http://wiki.eclipse.org/MoDisco/Components", "_blank", 2);
-	#$Nav->addCustomNav("Use Cases", "/MoDisco/useCases/", "_self", 3);
-	#$Nav->addCustomNav("Technologies", "/MoDisco/technologies/", "_self", 3);
-	#$Nav->addCustomNav("Infrastructure", "/MoDisco/infrastructure/", "_self", 3);
-	$Nav->addNavSeparator("Downloads", "/MoDisco/downloads/");
-	$Nav->addCustomNav("Installation", "/MoDisco/installation/", "_self", 3);
-	$Nav->addCustomNav("Update site", "/MoDisco/updates/", "_self", 3);
-	$Nav->addNavSeparator("Documentation", "/MoDisco/doc/");
-	$Nav->addCustomNav("Project Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=modeling.mdt.modisco", "_self", 3);
-	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/index.php/MoDisco", "_blank", 3);
-	$Nav->addNavSeparator("Community", ".");
-	$Nav->addCustomNav("SVN", "http://dev.eclipse.org/viewsvn/index.cgi/?root=Modeling_MODISCO", "_blank", 3);
-	$Nav->addCustomNav("Forum", "news://news.eclipse.org/eclipse.modisco", "_blank", 3);
-	$Nav->addCustomNav("Forum (Web)", "http://www.eclipse.org/forums/eclipse.modisco", "_blank", 3);
-	$Nav->addCustomNav("Submit a bug", "https://bugs.eclipse.org/bugs/enter_bug.cgi?product=MDT&component=MoDisco", "_blank", 3);
-	$Nav->addCustomNav("Open bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced;bug_status=UNCONFIRMED;bug_status=NEW;bug_status=ASSIGNED;bug_status=REOPENED;component=MoDisco;component=MoDisco-Contribution;component=MoDisco-Infrastructure;component=MoDisco-Technologies;component=MoDisco-UseCases;classification=Modeling;product=MDT", "_blank", 3);
-	$Nav->addCustomNav("Contributors", "http://www.eclipse.org/MoDisco/project-info/team.php", "_self", 3);
-	$Nav->addCustomNav("Interested Parties", "/MoDisco/interestedParties.php", "_self", 2);
-	$Nav->addCustomNav("Related Projects", "/MoDisco/relatedProjects.php", "_self", 2);
+	$Nav->addNavSeparator("MoDisco", "/modisco/");
+	$Nav->addCustomNav("Download", "http://wiki.eclipse.org/MoDisco/Installation", "_self", 3);
+	$Nav->addCustomNav("Documentation", "http://wiki.eclipse.org/MoDisco#Documentation", "_self", 3);
+	$Nav->addCustomNav("Support", "http://wiki.eclipse.org/MoDisco#Support", "_self", 3);
+	$Nav->addCustomNav("Involvement", "http://wiki.eclipse.org/MoDisco#Getting_involved", "_self", 3);
+	
+	$Menu->setMenuItemList(array());
+	$Menu->addMenuItem("Home", "/modisco/", "_self");
+	$Menu->addMenuItem("Download", "http://wiki.eclipse.org/MoDisco/Installation", "_self");
+	$Menu->addMenuItem("Documentation", "http://wiki.eclipse.org/MoDisco#Documentation", "_self");
+	$Menu->addMenuItem("Support", "http://wiki.eclipse.org/MoDisco#Support", "_self");
+	$Menu->addMenuItem("Developers", "http://wiki.eclipse.org/MoDisco#Getting_involved", "_self");
+	
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="modisco.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader("<link rel='alternate' type='application/rss+xml' title='MoDisco News' href='news/modiscoNewsArchive.rss'>");
+	
+	$App->Promotion = TRUE;
 	
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*******************************************************************************
- * Copyright (c) 2009 Eclipse Foundation and others.
+ * Copyright (c) 2009, 2014 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,11 @@
  *
  * Contributors:
  *    Grégoire Dupé (Mia-Software)
+ *	  Hugo Bruneliere (Inria)
  *******************************************************************************/
-																													require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());
-	
-	$localVersion = false;
-	
+
 	# Define these here, or in _projectCommon.php for site-wide values
-	$pageKeywords	= "eclipse, modeling, emf, modisco, reverse engineering, mde";
+	$pageKeywords	= "eclipse, modeling, emf, modisco, reverse engineering, modernization, mde";
 	$pageAuthor		= "Gregoire Dupe, Hugo Bruneliere";
 	$pageTitle 		= "MoDisco";
 	
@@ -29,6 +26,7 @@
 		$xmldoc->load("news/modiscoNewsArchive.rss");
 		return $xsl->transformToXML($xmldoc); 
 	}
+	
 	function getTwitter(){
 		$xsl = new Xsltprocessor();
 		$xsldoc = new DomDocument();
@@ -39,13 +37,11 @@
 		$xmldoc->load("http://search.twitter.com/search.atom?q=modisco");
 		return $xsl->transformToXML($xmldoc); 
 	}
+	
 	$html = file_get_contents('_index.html');
 	$news = getModiscoNews();
 	# TODO $twitter = getTwitter();
 	$html = str_replace("%%HEADLINES%%", $news, $html);
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>' . "\n\t");
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="modisco.css"/>' . "\n\t");
-	$App->AddExtraHtmlHeader("<link rel='alternate' type='application/rss+xml' title='MoDisco News' href='news/modiscoNewsArchive.rss'>");
-	$pageKeywords="MDA, MDE, MoDisco, Model Discovery, Quality Assurance, Documentation, Improvement, Migration, EMF, Eclipse";
+	
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
